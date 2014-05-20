46cdc6a9adf702ac6ee87b6efca2fe2402b8900e
illidan <illidan.firerage@qq.com>
2014-05-20 11:05:34 +0800

    create FirerageKit project

---
+++ FirerageKit/FirerageKit/FRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FRAppDelegate.h
+//  FirerageKit
+//
+//  Created by Aidian.Tang on 14-5-20.
+//  Copyright (c) 2014年 Illidan.Firerage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FirerageKit/Pods/Pods-FirerageKitTests-GHUnit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirerageKitTests_GHUnit : NSObject
+@end
+@implementation PodsDummy_Pods_FirerageKitTests_GHUnit
+@end
