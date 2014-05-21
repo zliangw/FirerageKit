a389b5bd9bbd5b4e8b165f9e204b93ee690e1d74
illidan <illidan.firerage@qq.com>
2014-05-21 17:58:08 +0800

    start send message controller feature

---
+++ FirerageKit/FirerageKit/FRSendMessageTableViewController/FRSendMessageViewController.h
@@ -0,0 +1,15 @@
+//
+//  FRSendMessageTableViewController.h
+//  FirerageKit
+//
+//  Created by Aidian.Tang on 14-5-21.
+//  Copyright (c) 2014年 Illidan.Firerage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRSendMessageViewController : UIViewController
+
+@property (nonatomic, strong, readonly) UITableView *messageTableView;
+
+@end
