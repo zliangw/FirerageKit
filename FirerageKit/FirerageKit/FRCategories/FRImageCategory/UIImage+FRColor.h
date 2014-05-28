be554080622b75cec6bacc382a925f6489c91b87
illidan <illidan.firerage@qq.com>
2014-05-28 13:05:41 +0800

    add FRImageCategory

---
+++ FirerageKit/FirerageKit/FRCategories/FRImageCategory/UIImage+FRColor.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+FRColor.h
+//  FirerageKit
+//
+//  Created by Aidian.Tang on 14-5-28.
+//  Copyright (c) 2014年 Illidan.Firerage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (FRColor)
+
++ (UIImage *)imageWithColor:(UIColor *)color;
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size andRoundSize:(CGFloat)roundSize;
+
+@end
