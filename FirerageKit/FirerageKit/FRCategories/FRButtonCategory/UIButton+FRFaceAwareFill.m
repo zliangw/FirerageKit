50bd7a5bd263476350531381ad940ed1f22ceb17
illidan <illidan.firerage@qq.com>
2014-06-10 15:13:06 +0800

    add cache

--- FirerageKit/FirerageKit/FRCategories/FRButtonCategory/UIButton+FRFaceAwareFill.m
+++ FirerageKit/FirerageKit/FRCategories/FRButtonCategory/UIButton+FRFaceAwareFill.m
@@ -7,6 +7,7 @@
 //
 
 #import "UIButton+FRFaceAwareFill.h"
+#import "SDImageCache.h"
 #import "objc/runtime.h"
 
 static char operationKey;
@@ -20,28 +21,47 @@ static char operationKey;
     [self setBackgroundImage:placeholder forState:state];
     
     if (url) {
-        __weak UIButton *wself = self;
-        id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadWithURL:url options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished) {
-            if (!wself) return;
-            dispatch_main_sync_safe(^{
-                __strong UIButton *sself = wself;
-                if (!sself) return;
-                if (image) {
-                    UIImage *newImage = [image cropWithProportion:proportion type:cropType];
-                    if (faceAwareFilled) {
-                        [newImage faceAwareFillWithSize:CGSizeMake(self.frame.size.width * 2, self.frame.size.height * 2) block:^(UIImage *faceAwareFilledImage) {
-                            [sself setBackgroundImage:faceAwareFilledImage forState:state];
-                        }];
-                    } else {
-                        [sself setBackgroundImage:newImage forState:state];
+        CGSize cropSize = CGSizeMake(self.frame.size.width * 2, self.frame.size.height * 2);
+        NSString *cacheKey = [NSString stringWithFormat:@"%@cacheForSize%@faceAwareFilled%dproportion%fcropType%d", url.absoluteString, NSStringFromCGSize(cropSize), faceAwareFilled, proportion, cropType];
+        UIImage *cacheImage = [[SDImageCache sharedImageCache] imageFromDiskCacheForKey:cacheKey];
+        if (cacheImage) {
+            [self setBackgroundImage:cacheImage forState:state];
+            if (completedBlock) {
+                completedBlock(cacheImage, nil, SDImageCacheTypeDisk);
+            }
+        } else {
+            __weak UIButton *wself = self;
+            id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadWithURL:url options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished) {
+                if (!wself) return;
+                dispatch_main_sync_safe(^{
+                    __strong UIButton *sself = wself;
+                    if (!sself) return;
+                    if (image) {
+                        UIImage *newImage = [image cropWithProportion:proportion type:cropType];
+                        if (faceAwareFilled) {
+                            [newImage faceAwareFillWithSize:cropSize cropType:cropType block:^(UIImage *faceAwareFilledImage) {
+                                [sself setBackgroundImage:faceAwareFilledImage forState:state];
+                                [[SDImageCache sharedImageCache] storeImage:faceAwareFilledImage forKey:cacheKey];
+                                if (completedBlock && finished) {
+                                    completedBlock(faceAwareFilledImage, error, cacheType);
+                                }
+                            }];
+                        } else {
+                            UIImage *cropImage = [image cropWithProportion:proportion type:cropType];
+                            [sself setBackgroundImage:cropImage forState:state];
+                            [[SDImageCache sharedImageCache] storeImage:cropImage forKey:cacheKey];
+                            if (completedBlock && finished) {
+                                completedBlock(cropImage, error, cacheType);
+                            }
+                        }
                     }
-                }
-                if (completedBlock && finished) {
-                    completedBlock(image, error, cacheType);
-                }
-            });
-        }];
-        objc_setAssociatedObject(self, &operationKey, operation, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+                    if (completedBlock && finished) {
+                        completedBlock(image, error, cacheType);
+                    }
+                });
+            }];
+            objc_setAssociatedObject(self, &operationKey, operation, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        }
     }
 }
 
--- FirerageKit/FirerageKit/FRCategories/FRImageViewCategory/UIImageView+FRFaceAwareFill.m
+++ FirerageKit/FirerageKit/FRCategories/FRImageViewCategory/UIImageView+FRFaceAwareFill.m
@@ -7,6 +7,7 @@
 //
 
 #import "UIImageView+FRFaceAwareFill.h"
+#import "SDImageCache.h"
 #import "objc/runtime.h"
 
 static char operationKey;
@@ -29,28 +30,42 @@ static char operationKey;
     self.image = placeholder;
     
     if (url) {
-        __weak UIImageView *wself = self;
-        id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadWithURL:url options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished) {
-            if (!wself) return;
-            dispatch_main_sync_safe(^{
+        CGSize cropSize = CGSizeMake(self.frame.size.width * 2, self.frame.size.height * 2);
+        NSString *cacheKey = [NSString stringWithFormat:@"%@cacheForSize%@faceAwareFilled%dproportion%fcropType%d", url.absoluteString, NSStringFromCGSize(cropSize), faceAwareFilled, proportion, cropType];
+        UIImage *cacheImage = [[SDImageCache sharedImageCache] imageFromDiskCacheForKey:cacheKey];
+        if (cacheImage) {
+            self.image = cacheImage;
+            if (completedBlock) {
+                completedBlock(cacheImage, nil, SDImageCacheTypeDisk);
+            }
+        } else {
+            __weak UIImageView *wself = self;
+            id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadWithURL:url options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished) {
                 if (!wself) return;
-                if (image) {
-                    if (faceAwareFilled) {
-                        [image faceAwareFillWithSize:CGSizeMake(self.frame.size.width * 2, self.frame.size.height * 2) block:^(UIImage *faceAwareFilledImage) {
-                            wself.image = faceAwareFilledImage;
-                            [wself setNeedsLayout];
-                        }];
-                    } else {
-                        wself.image = [image cropWithProportion:proportion type:cropType];
-                        [wself setNeedsLayout];
+                dispatch_main_sync_safe(^{
+                    if (!wself) return;
+                    if (image) {
+                        if (faceAwareFilled) {
+                            [image faceAwareFillWithSize:cropSize cropType:cropType block:^(UIImage *faceAwareFilledImage) {
+                                wself.image = faceAwareFilledImage;
+                                [[SDImageCache sharedImageCache] storeImage:faceAwareFilledImage forKey:cacheKey];
+                                if (completedBlock && finished) {
+                                    completedBlock(faceAwareFilledImage, error, cacheType);
+                                }
+                            }];
+                        } else {
+                            UIImage *cropImage = [image cropWithProportion:proportion type:cropType];
+                            wself.image = cropImage;
+                            [[SDImageCache sharedImageCache] storeImage:cropImage forKey:cacheKey];
+                            if (completedBlock && finished) {
+                                completedBlock(cropImage, error, cacheType);
+                            }
+                        }
                     }
-                }
-                if (completedBlock && finished) {
-                    completedBlock(image, error, cacheType);
-                }
-            });
-        }];
-        objc_setAssociatedObject(self, &operationKey, operation, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+                });
+            }];
+            objc_setAssociatedObject(self, &operationKey, operation, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        }
     }
 }
 
