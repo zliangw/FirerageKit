6b45db6265aed7457d8f06c6a1f270d91b9d13a8
jiangping.tang <jiangping.tang@zhenai.com>
2015-09-01 13:51:49 +0800

    更新date类别中的日期属性和方法

--- Firerage/FRCategories/Date/NSDate+FRUtils.h
+++ Firerage/FRCategories/Date/NSDate+FRUtils.h
@@ -22,4 +22,75 @@ typedef enum {
 - (NSString *)toStringWithDateFormatter:(NSDateFormatter *)dateFormatter;
 - (NSString *)toTimestampSince1970;
 
+// Relative dates from the current date
++ (NSDate *) dateTomorrow;
++ (NSDate *) dateYesterday;
++ (NSDate *) dateWithDaysFromNow: (NSInteger) days;
++ (NSDate *) dateWithDaysBeforeNow: (NSInteger) days;
++ (NSDate *) dateWithHoursFromNow: (NSInteger) dHours;
++ (NSDate *) dateWithHoursBeforeNow: (NSInteger) dHours;
++ (NSDate *) dateWithMinutesFromNow: (NSInteger) dMinutes;
++ (NSDate *) dateWithMinutesBeforeNow: (NSInteger) dMinutes;
+
+// Comparing dates
+- (BOOL) isEqualToDateIgnoringTime: (NSDate *) aDate;
+- (BOOL) isToday;
+- (BOOL) isTomorrow;
+- (BOOL) isYesterday;
+- (BOOL) isSameWeekAsDate: (NSDate *) aDate;
+- (BOOL) isThisWeek;
+- (BOOL) isNextWeek;
+- (BOOL) isLastWeek;
+- (BOOL) isSameMonthAsDate: (NSDate *) aDate;
+- (BOOL) isThisMonth;
+- (BOOL) isSameYearAsDate: (NSDate *) aDate;
+- (BOOL) isThisYear;
+- (BOOL) isNextYear;
+- (BOOL) isLastYear;
+- (BOOL) isEarlierThanDate: (NSDate *) aDate;
+- (BOOL) isLaterThanDate: (NSDate *) aDate;
+- (BOOL) isInFuture;
+- (BOOL) isInPast;
+
+// Date roles
+- (BOOL) isTypicallyWorkday;
+- (BOOL) isTypicallyWeekend;
+
+// Adjusting dates
+- (NSDate *) dateByAddingDays: (NSInteger) dDays;
+- (NSDate *) dateBySubtractingDays: (NSInteger) dDays;
+- (NSDate *) dateByAddingHours: (NSInteger) dHours;
+- (NSDate *) dateBySubtractingHours: (NSInteger) dHours;
+- (NSDate *) dateByAddingMinutes: (NSInteger) dMinutes;
+- (NSDate *) dateBySubtractingMinutes: (NSInteger) dMinutes;
+- (NSDate *) dateAtStartOfDay;
+
+// Retrieving intervals
+- (NSInteger) minutesAfterDate: (NSDate *) aDate;
+- (NSInteger) minutesBeforeDate: (NSDate *) aDate;
+- (NSInteger) hoursAfterDate: (NSDate *) aDate;
+- (NSInteger) hoursBeforeDate: (NSDate *) aDate;
+- (NSInteger) daysAfterDate: (NSDate *) aDate;
+- (NSInteger) daysBeforeDate: (NSDate *) aDate;
+- (NSInteger)distanceInDaysToDate:(NSDate *)anotherDate;
+
+// Decomposing dates
+@property (readonly) NSInteger nearestHour;
+@property (readonly) NSInteger hour;
+@property (readonly) NSInteger minute;
+@property (readonly) NSInteger seconds;
+@property (readonly) NSInteger day;
+@property (readonly) NSInteger month;
+@property (readonly) NSInteger week;
+@property (readonly) NSInteger weekday;
+@property (readonly) NSInteger nthWeekday; // e.g. 2nd Tuesday of the month == 2
+@property (readonly) NSInteger year;
+/***
+ * aim
+ ** Convert text according to time format to date
+ @pram
+ ** format : ex-- @"yyyy-MM-dd HH:mm:ss.SSS"
+ ***/
++ (NSDate *)dateFromString:(NSString *)dateString TimeFormat:(NSString*)format;
+
 @end
